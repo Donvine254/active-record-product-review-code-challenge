5f3c244a0226c1d8bafd90e61894c639bc1f7c8d
donvine mugendi <donvinemugendi@gmail.com>
2023-07-16 22:40:09 +0300

    error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :products ,through: :reviews
   has_many :reviews
+  has_many :products ,through: :reviews
+ 
   def favorite_product
     fav_review = reviews.order(star_rating: :desc).first
     fav_review&.product #this will help return nil if the user has no reviews
   end
   def remove_reviews(product)
-    reviews=Review.where(product_id: product.id)
-    reviews.destroy_all
-  end
+    if product.is_a?(Product) && products.include?(product)
+      reviews.where(product_id: product.id).destroy_all
+    else
+      puts "You have not reviewed this product."
+    end
+  end  
 end
 
